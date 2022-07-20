bc0e1fa47cd1149db67f85ce0b521f438a76a18c
RockyFu267 <453618045@qq.com>
2022-07-20 12:37:54 +0800

    update

--- basefunc/getOsInfo.go
+++ basefunc/getOsInfo.go
@@ -1,6 +1,7 @@
 package basefunc
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"runtime"
@@ -9,20 +10,39 @@ import (
 //GetHostInfo 获取基础信息
 func GetHostInfo() (HostInfo, error) {
 	var tempInfo HostInfo
-	tmpOs := runtime.GOOS
-	tmpArch := runtime.GOARCH
 
-	tempInfo.OperatingSystem = tmpOs
+	//获取架构并赋值
+	tmpArch := GetCPUArch()
 	tempInfo.CPUARCH = tmpArch
 
-	//获取主机名
-	tempHostName, err := os.Hostname()
+	//获取主机名并赋值
+	tempHostName, err := GetHostName()
 	if err != nil {
 		log.Println(err)
 		//***结束***
+		//改进  不能结束，继续收集其他信息
 		return tempInfo, err
 	}
 	tempInfo.HostName = tempHostName
 
+	tempInfo.getHypervisor()
+	fmt.Println()
+
 	return tempInfo, nil
+}
+
+//GetHostName 获取主机名
+func GetHostName() (string, error) {
+	res, err := os.Hostname()
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+	return res, nil
+}
+
+//GetCPUArch 获取cpu架构
+func GetCPUArch() string {
+	tmpArch := runtime.GOARCH
+	return tmpArch
 }
--- basefunc/type.go
+++ basefunc/type.go
@@ -2,7 +2,8 @@ package basefunc
 
 //OSInfo 主机信息
 type HostInfo struct {
-	OperatingSystem string `json:"operatingsystem"`
-	CPUARCH         string `json:"cpuarch"`
-	HostName        string `json:"hostname"`
+	OperatingSystem string `json:"operatingsystem,omitempty"`
+	CPUARCH         string `json:"cpuarch,omitempty"`
+	HostName        string `json:"hostname,omitempty"`
+	Hypervisor      string `json:"hypervisor,omitempty"`
 }
