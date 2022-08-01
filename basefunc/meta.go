e2bed45fb67f436a272c2990e8dfb44b63043d9f
RockyFu267 <453618045@qq.com>
2022-08-01 15:17:33 +0800

    add 生成json文件

--- basefunc/meta.go
+++ basefunc/meta.go
@@ -4,11 +4,11 @@ import "time"
 
 // Meta information.
 type Meta struct {
-	Version   string    `json:"version"`
-	Timestamp time.Time `json:"timestamp"`
+	Version   string `json:"version"`
+	Timestamp string `json:"timestamp"`
 }
 
 func (si *HostInfo) getMetaInfo() {
 	si.Meta.Version = Version
-	si.Meta.Timestamp = time.Now()
+	si.Meta.Timestamp = time.Now().Format("2006-01-02_15:04:05")
 }
--- main.go
+++ main.go
@@ -4,8 +4,12 @@ import (
 	bf "EnvCheck/basefunc"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
+	"math/rand"
 	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
@@ -27,4 +31,43 @@ func main() {
 	resJson, _ := json.MarshalIndent(res, "", " ")
 	fmt.Println(string(resJson))
 
+	//获取当前时间
+	timeStr := time.Now().Format("2006-01-02_15:04:05")
+	envFileName := "./" + timeStr + ".json"
+	//文件是否存在
+	checkDir, err := bf.CheckDir(envFileName)
+	if err != nil {
+		log.Println("Get jsonfile ERROR: ", err)
+		//***结束***
+		return
+	}
+	//设置随机数种子
+	rand.Seed(time.Now().UnixNano())
+	tmpStr := strconv.Itoa(rand.Intn(10000))
+
+	var file *os.File
+	if checkDir {
+		//使用追加模式打开文件
+		envFileNameTmp := "./" + timeStr + "_" + tmpStr + ".json"
+		file, err = os.Create(envFileNameTmp)
+		if err != nil {
+			fmt.Println("创建失败", err)
+			return
+		}
+	} else {
+		//不存在创建文件
+		file, err = os.Create(envFileName)
+		if err != nil {
+			fmt.Println("创建失败", err)
+			return
+		}
+	}
+	defer file.Close()
+	//写入文件
+	echoJson, err := io.WriteString(file, string(resJson))
+	if err != nil {
+		fmt.Println("写入错误：", err)
+		return
+	}
+	fmt.Println("写入成功", echoJson)
 }
