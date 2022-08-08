94b41ac386fb08173fbc6f68cb3c09bba5662885
RockyFu267 <453618045@qq.com>
2022-08-08 16:41:19 +0800

    update

--- basecmd/cmdinput_test.go
+++ basecmd/cmdinput_test.go
@@ -1,16 +1,16 @@
 package basecmd
 
 import (
-	"log"
-	"testing"
+	_ "log"
+	_ "testing"
 )
 
-func Test_CmdAndChangeDirToShow(t *testing.T) {
-	params := []string{"-l"}
-	err := CmdAndChangeDirToShow("/Users/fuao/Downloads/chart-test/test01", "ls", params)
-	if err != nil {
-		log.Println("cmd.StdoutPipe: ", err)
-		return
-	}
+// func Test_CmdAndChangeDirToShow(t *testing.T) {
+// 	params := []string{"-l"}
+// 	err := CmdAndChangeDirToShow("/Users/fuao/Downloads/chart-test/test01", "ls", params)
+// 	if err != nil {
+// 		log.Println("cmd.StdoutPipe: ", err)
+// 		return
+// 	}
 
-}
+// }
