24f17e69c19b2fdd330fdf562037e225c89682c2
RockyFu267 <453618045@qq.com>
2022-08-11 13:29:55 +0800

    删除无用方法

--- basecmd/cmdOutputRes_test.go
+++ basecmd/cmdOutputRes_test.go
@@ -7,19 +7,6 @@ import (
 	"testing"
 )
 
-func Test_CmdAndChangeDirToRes(t *testing.T) {
-	params := []string{}
-	res, err := CmdAndChangeDirToRes("/Users/fuao/Downloads/chart-test/test01", "ls", params)
-	if err != nil {
-		log.Println("cmd.StdoutPipe: ", err)
-		return
-	}
-	fmt.Println(res[0], len(res[0]))
-	str := strings.Replace(res[0], "\n", "", -1)
-	fmt.Println(str, len(str))
-
-}
-
 func Test_CmdAndChangeDirToResAllInOne(t *testing.T) {
 	res, err := CmdAndChangeDirToResAllInOne("/Users/fuao/Downloads/", "lscpi")
 	if err != nil {
@@ -31,15 +18,3 @@ func Test_CmdAndChangeDirToResAllInOne(t *testing.T) {
 	fmt.Println(str, len(str))
 
 }
-
-// func Test_CmdAndChangeDirToResFile(t *testing.T) {
-// 	err := CmdAndChangeDirToResFile("/Users/fuao/Desktop/code/github/SamllToolForDeploy/output/001.yaml", "/Users/fuao/Desktop/code/github/SamllToolForDeploy/", "helm install -n testtest doc-audit /Users/fuao/Desktop/code/github/SamllToolForDeploy/output/doc-audit --dry-run")
-// 	if err != nil {
-// 		log.Println("cmd.StdoutPipe: ", err)
-// 		return
-// 	}
-// 	// fmt.Println(res[0], len(res[0]))
-// 	// str := strings.Replace(res[0], "\n", "", -1)
-// 	// fmt.Println(str, len(str))
-// 	fmt.Println("Ok")
-// }
