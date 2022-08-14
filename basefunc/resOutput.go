a3a97afbc1950da588807deeb1fc3ffbb6d6da7b
RockyFu267 <453618045@qq.com>
2022-08-14 23:18:38 +0800

    添加压缩方法

---
+++ basefunc/resOutput.go
@@ -0,0 +1,30 @@
+package basefunc
+
+import (
+	"bytes"
+	"compress/zlib"
+	"io"
+)
+
+//encryptionString 压缩字符串
+func encryptionString(input []HostInfo) (res string) {
+	return res
+}
+
+//进行zlib压缩
+func DoZlibCompress(src []byte) []byte {
+	var in bytes.Buffer
+	w := zlib.NewWriter(&in)
+	w.Write(src)
+	w.Close()
+	return in.Bytes()
+}
+
+//进行zlib解压缩
+func DoZlibUnCompress(compressSrc []byte) []byte {
+	b := bytes.NewReader(compressSrc)
+	var out bytes.Buffer
+	r, _ := zlib.NewReader(b)
+	io.Copy(&out, r)
+	return out.Bytes()
+}
