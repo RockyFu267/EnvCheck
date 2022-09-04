6cf945b619d724bca7692cb69f97ea5fe447fda6
RockyFu267 <453618045@qq.com>
2022-09-04 18:13:10 +0800

    add 单元测试

--- controller/readConfig_test.go
+++ controller/readConfig_test.go
@@ -1,7 +1,9 @@
 package controller
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"testing"
 )
 
@@ -9,4 +11,17 @@ func Test_ReadConfig(t *testing.T) {
 	res := ReadConfig("/Users/fuao/Desktop/code/github/EnvCheck")
 	fmt.Println(res)
 	fmt.Println(res.MasterIP, res.MasterPort, res.RemotePath)
+}
+
+func Test_ReadResJson(t *testing.T) {
+	res, err := ReadResJson("/Users/fuao/Downloads/2022-09-04_17-47-47.json")
+	fmt.Println(err)
+	resJson, err := json.MarshalIndent(res, "", " ")
+	if err != nil {
+		if err != nil {
+			log.Println("解析数组错误", err)
+			return
+		}
+	}
+	fmt.Println(string(resJson))
 }
