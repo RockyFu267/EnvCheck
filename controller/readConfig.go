ffc9dfa3cf990f223341dc93c82230d008b87aff
RockyFu267 <453618045@qq.com>
2022-12-01 00:33:01 +0800

    update config

--- controller/readConfig.go
+++ controller/readConfig.go
@@ -1,17 +1,17 @@
 package controller
 
 import (
+	ebf "EnvCheck/basefunc"
 	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"log"
-
-	ebf "EnvCheck/basefunc"
 
 	"gopkg.in/yaml.v3"
 )
 
 //ReadConfig 读取外部配置文件
-func ReadConfig(input string) EnvConf {
+func ReadConfig(input string) (EnvConf, error) {
 	yamlFile, err := ioutil.ReadFile(input + "/config.yaml")
 	if err != nil {
 		log.Println("open config file error:", err)
@@ -26,13 +26,35 @@ func ReadConfig(input string) EnvConf {
 		if v.Port == "" {
 			confDemo.Host[k].Port = "22"
 		}
+		log.Println(k, confDemo.Disk.Type)
+		switch confDemo.Disk.Type {
+		case "none":
+			confDemo.Host[k].DiskCheck = "disable"
+		case "all":
+			if v.DiskCheck == "disable" {
+				continue
+			}
+			confDemo.Host[k].DiskCheck = "enable"
+			if v.DiskCheckPath == "" {
+				confDemo.Host[k].DiskCheckPath = confDemo.Disk.DiskCheckPath
+			}
+		case "custom":
+			if v.DiskCheck == "enable" && v.DiskCheckPath == "" {
+				return confDemo, errors.New("disk.type Error,please check parameter")
+			}
+			if v.DiskCheck == "" {
+				confDemo.Host[k].DiskCheck = "disable"
+			}
+		default:
+			return confDemo, errors.New("disk.type Error,please check parameter")
+		}
 	}
 	lenTmp := len(confDemo.RemotePath)
 	if string(confDemo.RemotePath[lenTmp-1]) != "/" {
 		confDemo.RemotePath = confDemo.RemotePath + "/"
 	}
 
-	return confDemo
+	return confDemo, nil
 
 }
 
